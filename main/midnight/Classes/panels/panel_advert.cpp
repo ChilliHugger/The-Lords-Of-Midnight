043e856d252feeb761af1b9531bfd5e9e0f70781
Chris Wild <chris.wild@chillihugger.com>
2018-11-15 23:19:32 +0000

    Advert screen

--- main/midnight/Classes/panels/panel_advert.cpp
+++ main/midnight/Classes/panels/panel_advert.cpp
@@ -16,6 +16,22 @@
 //#include "../system/panelmanager.h"
 #include "../ui/uihelper.h"
 
+#if defined(_LOM_)
+#if defined(_OS_DESKTOP_)
+#define SCREEN_ADVERT   "screens/misc/lom_advert_for_desktops.png"
+#else
+#define SCREEN_ADVERT   "screens/misc/lom_advert_for_mobiles.png"
+#endif
+#endif
+
+#if defined(_DDR_)
+#if defined(_OS_DESKTOP_)
+#define SCREEN_ADVERT   "screens/misc/ddr_advert_for_desktops.png"
+#else
+#define SCREEN_ADVERT   "screens/misc/ddr_advert_for_mobile.png"
+#endif
+#endif
+
 
 bool panel_advert::init()
 {
@@ -24,7 +40,9 @@ bool panel_advert::init()
         return false;
     }
     
-    setBackground(_clrBlue);
+    setBackgroundToWidth(SCREEN_ADVERT);
+    
+    Exit(5.0);
     
     return true;
 }
