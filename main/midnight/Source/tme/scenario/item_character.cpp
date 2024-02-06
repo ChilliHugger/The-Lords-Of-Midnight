e5f94cd9e0e08645ef3ae691cace3948137c7597
Francisco Assis Rosa <fassisrosa@hotmail.com>
2024-02-05 22:08:07 -0500

    Recruit Fey rule: on/off/lord of dreams required

--- main/midnight/Source/tme/scenario/item_character.cpp
+++ main/midnight/Source/tme/scenario/item_character.cpp
@@ -374,17 +374,27 @@ namespace tme {
                 // TODO: recruiting logic
                 return true ;
             }else{
-                if(mx->isRuleEnabled(RF_LOM_UNRECRUITABLE_FEY)
-                    && character->Race() == RA_FEY)
-                {
+                if ( character->Race() == RA_FEY) {
+                    // recruiting of Feys 'off', cannot recruit
+                    if (mx->isRuleEnabled(RF_LOM_FEY_RECRUIT_OFF)) {
+                        return false;
+                    }
+                    
                     // In the novel, Feys only join the cause after 
                     // the Lord of Dreams joins.
                     // The Lord of Dreams is required for recruiting Fey, 
-                    // he can always be recruited and other Fey can only
-                    // be recruited if he has already been recruited
-                    return character->Id() == mx->scenario->dreams->Id() ||
-                        mx->scenario->dreams->IsRecruited();
+                    // he can always be recruited and other Fey can
+                    // be recruited by all if he has already been recruited
+                    if (mx->isRuleEnabled(RF_LOM_FEY_RECRUIT_NOVEL) && 
+                        character->Id() != mx->scenario->dreams->Id()) {
+                        // Fey, novel mode and not Lord of Dreams?
+                        // Fey can be recruited by anyone if Lord of Dreams has been recruited
+                        return mx->scenario->dreams->IsRecruited();
+                    }
                 }
+                // check that 'this' character's RecruitingKey
+                // can recruit 'character' by checking who can 
+                // recruit it in flags 'RecruitedBy'
                 if ( character->RecruitedBy & RecruitingKey )
                     return true ;
             }
