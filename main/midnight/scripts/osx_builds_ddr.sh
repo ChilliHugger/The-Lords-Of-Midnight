9f46d5dbe4943acf412c63645c0a74e842e5abce
Chris Wild <chris@icemark.com>
2024-02-17 11:15:41 +0000

    Update Scripts

--- main/midnight/scripts/osx_builds_ddr.sh
+++ main/midnight/scripts/osx_builds_ddr.sh
@@ -1,22 +1,13 @@
-find . -type f -name '.DS*' -delete 
+find . -type f -name '.DS*' -delete
 
-rm -r mac-build-ddr
-mkdir mac-build-ddr
-cd mac-build-ddr
-cmake .. -GXcode -DTME:string=DDR
-attr -w com.apple.xcode.CreatedByBuildSystem true build
-cd ..
+rm -r Builds/mac-ddr
+mkdir Builds/mac-ddr
+axmol build -configOnly -p osx -a x64 -xc '-DTME=DDR,-BBuilds/mac-ddr'
 
-rm -r mac-tests-build-ddr
-mkdir mac-tests-build-ddr
-cd mac-tests-build-ddr
-cmake .. -GXcode -DTME:string=DDR -DTESTS=YES
-attr -w com.apple.xcode.CreatedByBuildSystem true build
-cd ..
+rm -r Builds/mac-tests-ddr
+mkdir Builds/mac-tests-ddr
+axmol build -configOnly -p osx -a x64 -xc '-DTME=DDR,-DTESTS=YES,-BBuilds/mac-tests-ddr'
 
-rm -r ios-build-ddr
-mkdir ios-build-ddr
-cd ios-build-ddr
-cmake .. -GXcode -DCMAKE_SYSTEM_NAME=iOS -DTME:string=DDR
-attr -w com.apple.xcode.CreatedByBuildSystem true build
-cd ..
+rm -r Builds/ios-ddr
+mkdir Builds/ios-ddr
+axmol build -configOnly -p ios -a arm64 -xc '-DTME=DDR,-BBuilds/ios-ddr'
