c4a09dd9a0da0fb219104457ff030bbf30c03c61
Chris Wild <chris@icemark.com>
2024-02-19 00:12:56 +0000

    Create Android LoM project

--- main/midnight/Source/platform/android/Extensions-android.cpp
+++ main/midnight/Source/platform/android/Extensions-android.cpp
@@ -15,18 +15,18 @@ namespace chilli
 {
     namespace extensions
     {
-        const std::string utilsClassName = "com.chillihugger.extensions.Utils";
+        const std::string utilsClassName = "com.chilli.extensions.Utils";
 
         std::string getBuildNo()
         {
             // Build is xyyyzzz, where xxx=major, yyy=minor, zzz=build
-            auto version =  JniHelper::callStaticStringMethod(utilsClassName, "getBuildNo");
+            auto version =  JniHelper::callStaticStringMethod(utilsClassName.c_str(), "getBuildNo");
             if(version.size()>3) {
                 return version.substr(version.size() - 3);
             }
             return version;
         }
-		
+
         std::string getVersion()
         {
             return Application::getInstance()->getVersion();
