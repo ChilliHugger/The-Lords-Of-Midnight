60ce03ca9d45c1cc7ae578d1fe4ec4ba2511f060
Chris Wild <chris@icemark.com>
2025-06-06 17:58:01 +0100

    Update Build to 2.0.11 (56)

--- main/midnight/scripts/osx_builds_ddr.sh
+++ main/midnight/scripts/osx_builds_ddr.sh
@@ -2,7 +2,8 @@ find . -type f -name '.DS*' -delete
 
 rm -r Builds/mac-ddr
 mkdir Builds/mac-ddr
-axmol build -configOnly -p osx -xc '-DTME=DDR,-BBuilds/mac-ddr,-DCMAKE_OSX_ARCHITECTURES=$(ARCHS_STANDARD)'
+axmol build -configOnly -p osx -a x64 -xc '-DTME=DDR,-BBuilds/mac-ddr'
+#axmol build -configOnly -p osx -xc '-DTME=DDR,-BBuilds/mac-ddr,-DCMAKE_OSX_ARCHITECTURES=$(ARCHS_STANDARD)'
 
 rm -r Builds/mac-tests-ddr
 mkdir Builds/mac-tests-ddr
